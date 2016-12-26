5a3ef84c3d907d2afcc7e4d01d1307bc08542433
Muhammad Wildan Suyuti <em.wildan.es@gmail.com>
2016-12-26 16:52:23 +0700

    first commit

--- app/src/main/java/com/mits/kakaroto/volleyjson/MainActivity.java
+++ app/src/main/java/com/mits/kakaroto/volleyjson/MainActivity.java
@@ -11,18 +11,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView results;
-    //private String JsonURL = "http://api.androidhive.info/volley/person_object.json";
-    private String JsonURL = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=49a941acf9da4909bf7f08284debe208";
-    private String data = "";
-    private RequestQueue requestQueue;
-    private JsonObjectRequest obreq;
-    private String name, email, home, mobile;
+    TextView results;
+
+    String JsonURL = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=49a941acf9da4909bf7f08284debe208";
+    JSONArray artticle = null;
+    private String author;
+
+    String data = "";
+    RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,25 +34,23 @@ public class MainActivity extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
 
         results = (TextView) findViewById(R.id.jsonData);
-        initJsonObject();
-    }
 
-    private void initJsonObject(){
-        obreq = new JsonObjectRequest(Request.Method.GET, JsonURL,
+        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonURL,
                 new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
 
-                            name = response.getString("source");
-//                            email = response.getString("email");
+                            String status = response.get("status").toString();
+                            artticle = response.getJSONArray("articles");
 
-//                            JSONObject phone = response.getJSONObject("articles");
-//                            home = phone.getString("author");
-//                            mobile = phone.getString("mobile");
+                            for (int i = 0; i< artticle.length(); i++){
+                                JSONObject obj = artticle.getJSONObject(i);
+                                author = obj.getString("author");
+                            }
 
-                            data += "Name: ";
+                            data += "Color Name: " + status+ " \n"+ author;
 
                             results.setText(data);
                         }
